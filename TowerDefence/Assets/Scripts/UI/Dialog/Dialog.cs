ed4f7ab605619255ceca2487d102cc3924d213b7
Donghyen-Lim <99638155+DongDongLim@users.noreply.github.com>
2024-03-30 11:00:07 +0900

    dialog

---
+++ TowerDefence/Assets/Scripts/UI/Dialog/Dialog.cs
@@ -0,0 +1,74 @@
+using UnityEngine;
+using UnityEngine.UI;
+using UniRx;
+
+public enum EDialogActiveState
+{
+    None = 0,
+    Activating,
+    Active,
+    DeActivating,
+    DeActive,
+}
+
+public class Dialog : MonoBehaviour
+{
+    #region Varialbles
+
+    [SerializeField] protected Button[] _closeButtons;
+
+    protected EDialogActiveState _activeState;
+    public EDialogActiveState ActiveState => _activeState;
+
+    #endregion
+
+    #region  Mathods
+
+    private void Awake()
+    {
+        foreach (var closeButton in _closeButtons)
+        {
+            closeButton.OnClickAsObservable().Subscribe(_ => DeActive());
+        }
+    }
+
+    private void OnEnable()
+    {
+
+    }
+
+    private void OnDisable()
+    {
+
+    }
+
+    public virtual void Active()
+    {
+        if (_activeState == EDialogActiveState.Activating)
+            return;
+        EveActive();
+        gameObject.SetActive(true);
+        _activeState = EDialogActiveState.Active;
+    }
+
+    protected virtual void EveActive()
+    {
+        _activeState = EDialogActiveState.Activating;
+    }
+
+    public virtual void DeActive()
+    {
+        if (_activeState == EDialogActiveState.DeActivating)
+            return;
+        EveDeActive();
+        gameObject.SetActive(false);
+        _activeState = EDialogActiveState.DeActive;
+    }
+
+    protected virtual void EveDeActive()
+    {
+        _activeState = EDialogActiveState.DeActivating;
+    }
+
+    #endregion
+}
