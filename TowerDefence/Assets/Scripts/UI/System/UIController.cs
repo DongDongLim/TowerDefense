45ee35460b192a5295cf58d192c1569a168a7ec5
Donghyen-Lim <99638155+DongDongLim@users.noreply.github.com>
2024-04-06 21:17:15 +0900

    uicontroller

--- TowerDefence/Assets/Scripts/UI/System/UIController.cs
+++ TowerDefence/Assets/Scripts/UI/System/UIController.cs
@@ -1,21 +1,59 @@
-using System;
-using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UniRx;
 using Cysharp.Threading.Tasks;
+using System;
 
-public class UIController : MonoBehaviour
+public sealed class UIController : Controller
 {
     [SerializeField] private RectTransform _uiParent;
+    private Dictionary<string, List<GameObject>> _activeUIObjects = new();
+    private Dictionary<string, Stack<GameObject>> _deActiveUIObjects = new();
 
-    void Awake()
+    protected override void Awake()
     {
-        UIEventManager.Instance.eventObservable.
-        Where(eventArg => eventArg.Key == EUIEventType.Open).
+        _controllerType = EControllerType.UI;
+        
+        GameManager.Instance.UIEventSubject.GetUIEventSubject(EUIEventType.Enable).
+        Select(eventArg => eventArg as UIEventArgument_Enable).
+        Where(eventArg => eventArg.isActive == true).
         Subscribe(eventArg =>
         {
-            AddressableUtil.InstantiateResource(eventArg.Value, _uiParent, null).Forget();
+            ActivateObject(eventArg.addressKey);
         }).AddTo(gameObject);
+    }
+
+    private void ActivateObject(string key)
+    {
+        if (_activeUIObjects.TryGetValue(key, out var activeObjects) == false)
+        {
+            _activeUIObjects.Add(key, new());
+        }
+
+        if (_deActiveUIObjects.TryGetValue(key, out var deActiveObjects) == false ||
+            deActiveObjects.TryPop(out var obj) == false)
+        {
+            AddressableUtil.InstantiateResource(key, _uiParent, activeObject => activeObjects.Add(activeObject)).Forget();
+            return;
+        }
+
+        obj.SetActive(true);
+
+        activeObjects.Add(obj);
+    }
+
+    private void DeActivateObject(string key, GameObject gameObject)
+    {
+        if(_deActiveUIObjects.TryGetValue(key, out var deActiveObjects) == false)
+        {
+            _deActiveUIObjects.Add(key, new());
+        }
+
+        if(_activeUIObjects.TryGetValue(key, out var activeObjects) == false ||
+            activeObjects.Contains(gameObject) == false)
+            throw new InvalidProgramException($"Not Active Object. {gameObject.name}");
+        
+        deActiveObjects.Push(gameObject);
+        activeObjects.Remove(gameObject);
     }
 }
